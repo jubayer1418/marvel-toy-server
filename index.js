66a2cb28864e31a8fa5e7ba6447771593face7e0
jubayer1418 <skjubayer1418@gmail.com>
2023-05-19 08:03:13 +0600

    add get method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxmnpk6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +24,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const allToysCollection = client.db("marvelToy").collection("toys");
+    app.get("/allToys", async (req, res) => {
+      console.log(req.query.sellerEmail);
+      let query = {};
+      if (req.query?.sellerEmail) {
+        query = { sellerEmail: req.query.sellerEmail };
+      }
+      const result = await allToysCollection.find(query).toArray();
+
+      res.send(result);
+    });
+    app.get("/allToys/:id", async (req, res) => {
+      console.log(req.params.id);
+      const query = {
+        _id: new ObjectId(req.params.id),
+      };
+      const result = await allToysCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/allToys", async (req, res) => {
+      const toys = req.body;
+      const result = await allToysCollection.insertOne(toys);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
